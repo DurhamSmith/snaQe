33755da3451c9eba746f72e9d45d3188cbbc14bd
Durham Smith <durham@futurefy.com>
2019-07-18 23:30:24 -0400

    please kill me now

--- snaqe.py
+++ snaqe.py
@@ -1,4 +1,8 @@
 #Snake Tutorial Python
+import dwave
+from pyqubo import Binary
+from dwave.system import DWaveSampler, EmbeddingComposite
+from dimod import ExactSolver
 from pyqubo import Binary
 import networkx as nx
 #import dwave_networkx as dnx
@@ -6,8 +10,14 @@ import math
 import random
 import pygame
 import tkinter as tk
+import dimod
 from tkinter import messagebox
 
+SIGMA= 2
+CHI = 0
+LAMBDA = 2
+MU = 2
+GAMMA = 2 
 class cube(object):
     rows = 20
     w = 500
@@ -101,7 +111,7 @@ class snake(object):
         pass
 
     def get_snake_unconnected_graph(self):
-        grid_size = 4
+        grid_size = 3
         # G = nx.Graph()
         # for i in range(grid_size):
         #     for j in range(grid_size):
@@ -320,10 +330,30 @@ class PathSolver():
         self.apple = apple_pos
         self.graph = graph.copy()
         self.vars = self.create_vars()
-        self.one_body_terms()
-        self.two_body_terms()
+        self.qubo = self.get_qubo()
+        self.run_dwave()
+
+    def run_dwave(self):
+        #sampler = DWaveSampler().sample_qubo(self.qubo)
+        print(len(self.qubo))
+        Dwavesolver = EmbeddingComposite(DWaveSampler())
+        sampleset = Dwavesolver.sample_qubo(self.qubo, num_reads=1000)
+        print(sampleset)
+  #      Q.update(coupler_strengths)
+        # Sample once on a D-Wave system and print the returned sample
+        #response = DWaveSampler().sample_qubo(Q, num_reads=1)
 
     
+        #print(sampleset)
+        
+    def get_qubo(self):
+        H= self.one_body_terms() + self.two_body_terms() + self.get_justin_trubo()
+        model = H.compile()
+        #print(model)
+        qubo, offset = model.to_qubo()
+        print(f'QUBO:\n {qubo}')
+        return qubo
+        
     def create_vars(self):
         vars ={}
         for i, edge in enumerate(self.graph.edges()):
@@ -335,42 +365,38 @@ class PathSolver():
         #twobody head terms
         H=0
         H1=0
-        lambda_ = 2
         for head_edge_1 in self.head_edges:
             for head_edge_2 in self.head_edges:
                 if head_edge_1 == head_edge_2:
-                    H1 += lambda_*self.vars[head_edge_1]*self.vars[head_edge_2]
+                    H1 += LAMBDA*self.vars[head_edge_1]*self.vars[head_edge_2]
                 else:
-                    H1 += 0.5*lambda_*self.vars[head_edge_1]*self.vars[head_edge_2]
+                    H1 += LAMBDA*self.vars[head_edge_1]*self.vars[head_edge_2]
 
         #apple twobody terms
         H2=0
-        gamma = 2
         for apple_edge_1 in self.apple_edges:
             for apple_edge_2 in self.apple_edges:
                 if apple_edge_1 == apple_edge_2:
-                    H2 +=gamma*self.vars[apple_edge_1]*self.vars[apple_edge_2]
+                    H2 +=GAMMA*self.vars[apple_edge_1]*self.vars[apple_edge_2]
                 else:
-                    H2 += 0.5*gamma*self.vars[apple_edge_1]*self.vars[apple_edge_2]
+                    H2 += GAMMA*self.vars[apple_edge_1]*self.vars[apple_edge_2]
 
         #make sure graph is corrected
         H3=0
-        mu= 2
         for edge in self.graph.edges():
             for nodal_edge in self.graph.edges(self.get_valid_key(edge)[0]):
                 if self.get_valid_key(edge)[0] == self.get_valid_key(nodal_edge)[0]:
                     pass
                 else:
-                    H3 -= mu*self.vars[self.get_valid_key(nodal_edge)]*self.vars[self.get_valid_key(edge)]
+                    H3 -= MU*self.vars[self.get_valid_key(nodal_edge)]*self.vars[self.get_valid_key(edge)]
             for nodal_edge in self.graph.edges(self.get_valid_key(edge)[1]):
                 if self.get_valid_key(edge)[1] == self.get_valid_key(nodal_edge)[1]:
                     pass
                 else:
-                    H3 -= mu*self.vars[self.get_valid_key(nodal_edge)]*self.vars[self.get_valid_key(edge)]
+                    H3 -= MU*self.vars[self.get_valid_key(nodal_edge)]*self.vars[self.get_valid_key(edge)]
 
         #manby body terms
         H4=0
-        chi = 2
         for edge_1 in self.graph.edges():
             e1 = self.get_valid_key(edge_1)
             if e1 in self.apple_edges or e1 in self.head_edges:
@@ -381,14 +407,34 @@ class PathSolver():
                     if e2 in self.apple_edges or e2 in self.head_edges:
                         pass
                     elif e1==e2:
-                        H4 += -2*chi*self.vars[e1]*self.vars[e2]
+                        H4 += -2*CHI*self.vars[e1]*self.vars[e2]
                     else:
-                        H4 += -1*chi*self.vars[e1]*self.vars[e2]
+                        H4 += -2*CHI*self.vars[e1]*self.vars[e2]
 
         H = H1+H2+H3+H4
         return H
                 
-
+    def get_justin_trubo(self):
+        H=0
+        for edge_1 in self.graph.edges():
+            e1 = self.get_valid_key(edge_1)
+            if e1 in self.apple_edges or e1 in self.head_edges:
+                pass
+            else:
+                for edge_2 in self.graph.edges():
+                    e2 = self.get_valid_key(edge_2)
+                    if e2 in self.apple_edges or e2 in self.head_edges:
+                        pass
+                    else:
+                        for edge_3 in self.graph.edges():
+                            e3 = self.get_valid_key(edge_3)
+                            if e3 in self.apple_edges or e3 in self.head_edges:
+                                pass
+                            else:
+                                H += -2*CHI*self.vars[e1]*self.vars[e2]*self.vars[e3]
+        #print(H)
+        return H
+        
             
 
     def get_node(self, tup): 
@@ -413,29 +459,26 @@ class PathSolver():
 
         #Links around the head term
         H2=0
-        lambda_=2
         self.head_edges = []
         for edge in self.graph.edges(self.head):
             head_edge = self.get_valid_key(edge)
-            H2 -= 2*lambda_*self.vars[head_edge]
+            H2 -= 2*LAMBDA*self.vars[head_edge]
             self.head_edges.append(head_edge)
 
         #Links around the apple term
         H3=0
-        gamma=2
         self.apple_edges=[]
         for edge in self.graph.edges(self.apple):
             apple_edge = self.get_valid_key(edge)
-            H3 -= 2*gamma*self.vars[apple_edge]
+            H3 -= 2*GAMMA*self.vars[apple_edge]
             self.apple_edges.append(apple_edge)
 
         #Links from 3body bulk term
         H4=0
-        chi=2
         for edge in self.graph.edges():
             if edge in self.head_edges or edge in self.apple_edges:
                 pass
-            H4 -= 2*chi*self.vars[self.get_valid_key(edge)]
+            H4 -= 2*CHI*self.vars[self.get_valid_key(edge)]
 
         H= H1 + H2 + H3 + H4
 
@@ -460,12 +503,11 @@ def main():
     width = 500
     rows = 10
     #win = pygame.display.set_mode((width, width))
-    s = snake((0,255,0), (2,2))
+    s = snake((0,255,0), (1,1))
 #    snack = cube(randomSnack(rows, s), color=(255,0,0))
-    snack = cube(randomSnack(4, s), color=(255,0,0))
+    snack = cube(randomSnack(3, s), color=(255,0,0))
     flag = True
 
-    s.addCube()
     s.addCube()
     s.snake_to_graph()
     H=s.get_snake_unconnected_graph()
@@ -474,7 +516,8 @@ def main():
     H.add_node((11,11))
     H.add_edge((10,10),(11,10))
     H.add_edge((11,10),(11,11))
-    ps=PathSolver(s.snake_to_graph(), s.body[0].pos, snack.pos)
+    
+    ps=PathSolver(s.snake_to_graph(), s.body[0].pos, (1, 2))
 
 #    s.graph_to_moves(H)
 
